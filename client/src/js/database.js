ac0cb6012c84e28b256d07726220ef8b619b0924
Adam-Smart <adams8632@hotmail.co.uk>
2023-08-25 02:47:02 +0100

    fixed webpac.config and installed babel-loader

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -4,28 +4,40 @@ const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+        console.log('Found jate databasae');
         return;
       }
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      console.log('Made database');
     },
   });
 
-export const putDb = async (content) => console.error('Add to database');
-const jateDb = await openDB('jate', 1);
-const tx = jateDb.transaction('jate', 'readwrite');
-const store = tx.objectStore('jate');
-const request = store.put({ id: 1, content: content });
-const result = await request;
-console.log('Data added to database', result);
+export const putDb = async (content) => {
+  console.log('Sent to database');
 
-
-export const getDb = async () => { console.log('GET from the database');
   const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readonly');
+
+  const tx = jateDb.transaction('jate', 'readwrite');
+
   const store = tx.objectStore('jate');
+
+  const request = store.put({ id: 1, content: content });
+
+  const result = await request;
+  console.log('Saved data', result);
+}
+
+export const getDb = async () => {
+  console.log('Get data');
+
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
   const request = store.get(1);
+
   const result = await request;
   return result.value;
 }
